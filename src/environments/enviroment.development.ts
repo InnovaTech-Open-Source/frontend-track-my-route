2a9416d76086eef2113ed798c064346909665c18
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2024-06-25 14:05:53 -0500

    hotfix: changed serverPath link

--- src/environments/enviroment.development.ts
+++ src/environments/enviroment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
     // Server Base Path for Fake REST API
     // serverBasePath: 'http://localhost:3000/api/v1'
     // Server Base Path for Spring Boot REST API
-    serverBasePath: 'http://localhost:8080/api/v1'
+    serverBasePath: 'http://40.71.173.200/api/v1'
 };
--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    serverBasePath: "http://localhost:8080/api/v1"
+    serverBasePath: 'http://40.71.173.200/api/v1'
 };
